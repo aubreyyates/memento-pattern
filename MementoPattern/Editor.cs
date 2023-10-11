9bfde334f8c43373ef9b5a6fb349d6702885cdcb
Aubrey Yates <aubreyyates999@hotmail.com>
2023-10-10 22:51:09 -0500

    Add comments to the Editor, EditorState, and History classes.

--- MementoPattern/Editor.cs
+++ MementoPattern/Editor.cs
@@ -1,24 +1,47 @@
 ﻿namespace MementoPattern
 {
+    /// <summary>
+    /// The Editor class represents the element that will take in and store content. It is also known as the 
+    /// originator.
+    /// </summary>
     public class Editor
     {
+        /// <summary>
+        /// The content.
+        /// </summary>
         private string Content;
 
+        /// <summary>
+        /// Creates an EditorState to hold a snapshot of the current content and returns it.
+        /// </summary>
+        /// <returns>The EditorState that contains the current content.</returns>
         public EditorState CreateState()
         {
             return new EditorState(Content);
         }
 
+        /// <summary>
+        /// Restores the content to that of a given EditorState's held content.
+        /// </summary>
+        /// <param name="editorState">The EditorState.</param>
         public void Restore(EditorState editorState)
         {
             Content = editorState.GetContent();
         }
 
+        /// <summary>
+        /// Returns the content.
+        /// </summary>
+        /// <returns>The content to return.</returns>
         public string GetContent() 
         { 
             return Content;
         }
 
+        /// <summary>
+        /// Sets the content.
+        /// </summary>
+        /// <param name="content">The content to be set.</param>
         public void SetContent(string content)
         {
             Content = content;
--- MementoPattern/History.cs
+++ MementoPattern/History.cs
@@ -1,20 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MementoPattern
+﻿namespace MementoPattern
 {
+    /// <summary>
+    /// The History class holds all of the snapshots of content. It is also known as the Caretaker.
+    /// </summary>
     public class History
     {
+        /// <summary>
+        /// The list of EditorStates that hold the content snapshots.
+        /// </summary>
         private List<EditorState> editorStates = new List<EditorState>();
 
+        /// <summary>
+        /// Adds a new EditorState to the list of EditorStates.
+        /// </summary>
+        /// <param name="editorState">The EditorState to add.</param>
         public void Push(EditorState editorState) 
         { 
             editorStates.Add(editorState);
         }
 
+        /// <summary>
+        /// Removes the last EditorState, then returns the new last EditorState.
+        /// </summary>
+        /// <returns>The new last EditorState.</returns>
         public EditorState Pop()
         {
             editorStates.Remove(editorStates.Last());
